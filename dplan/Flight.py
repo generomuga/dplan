07b28fd622e8ec8964b268f91ea32e664e7e2430
Romuga <GRomuga@cgiar.org>
2018-01-18 13:51:54 +0800

    Initial commit

---
+++ dplan/Flight.py
@@ -0,0 +1,28 @@
+class Flight(object):
+
+	def __init__(self):
+		pass
+
+	def setCorners(self, **kwargs):
+		self.corner1 = kwargs['corner1']
+		self.corner2 = kwargs['corner2']
+		self.corner3 = kwargs['corner3']
+		self.corner4 = kwargs['corner4']
+
+	def setPartition(self, **kwargs):
+		self.x_partition = kwargs['x']
+		self.y_partition = kwargs['y']
+
+	def say(self):
+		print self.corner1
+
+#instance = Flight(corner1=(14.168087,121.255039), 
+#				  corner2=(14.168399,121.255377), 
+#				  corner3=(14.167741,121.256045),
+#				  corner4=(14.167442,121.255707))
+
+#instance.setPartition(x=3, y=4)
+
+instance = Flight()
+instance.setCorners(corner1=(14.168087,121.255039))
+instance.say()
