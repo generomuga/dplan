1745c5d972decd35287ed83a9f2cd40a1a7298d2
Romuga <GRomuga@cgiar.org>
2018-01-19 13:26:13 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dplan',
+  packages = ['dplan'], 
+  version = '1.0',
+  description = 'Drone flight plan generator for FLYLITCHI',
+  author = 'Gene Romuga',
+  author_email = 'generomuga12693@gmail.com',
+  url = 'https://github.com/generomuga/dplan', 
+  download_url = 'https://github.com/generomuga/dplan/releases/tag/1.0',
+  keywords = ['drone', 'flightplan', 'flylitchi'], 
+  classifiers = [],
+)
